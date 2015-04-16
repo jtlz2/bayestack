a4f6cdddbb3048e362e91044e9bf002ca6f12500
jtlz2 <jtlz2@astro.columbia.edu>
2015-04-16 13:57:27 +0200

    comments

--- bayestack.py
+++ bayestack.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python
 
-
 """
+Execute this code as
+
+mpirun -np 4 ./bayestack.py bayestack_settings.py
+
 """
 
 import os
 from bayestack_settings import *
 from bayestackClasses import *
 import pymultinest
-
-#-------------------------------------------------------------------------------
-
-def myprior(cube,ndim,nparams):
-    cube=model.transform(cube)
-    return
-
-def myloglike(cube,ndim,nparams):
-    if not model.checkSimPriorOK(): return -1.0e99
-    realisation=model.realise(cube)
-    loglike=poissonLhood(data,realisation)
-    return loglike
 
 #-------------------------------------------------------------------------------
 
