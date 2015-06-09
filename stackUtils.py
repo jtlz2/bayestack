e1854a2037e5b15e8357a328730883110752b36e
jtlz2 <jtlz2@astro.columbia.edu>
2015-06-09 13:18:53 +0200

    Added secateur, a function to apply cuts to a catalogue based on a supplied dictionary

--- stackUtils.py
+++ stackUtils.py
@@ -7,7 +7,7 @@ May 2015
 
 """
 
-
+import numpy
 import pyfits
 from profile_support import profile
 
@@ -30,4 +30,32 @@ def readRadio(fradio=None):
 
 #-------------------------------------------------------------------------------
 
+def secateur(incat,BINCOL,cutsDict,numNoiseZone):
+    """
+    Make cuts on the catalogue here
+    Excise stars - 0, 1, -1
+    Select AGNn - 0, 1, 2
+    Select XXX
+    Split into noise zones by noise bin value
+    Dictionary format: label:[flag,column]
+    """
+
+    for cut,cval in cutsDict.iteritems():
+        if cut in ['star','lacy','stern','donley']:
+            [ccol,cnum]=cval
+            #print cut,cnum,ccol
+            if cnum < 0: # i.e. no cut
+                incat=incat
+            else: # leave 0 or 1
+                expr=numpy.where(incat[:,ccol].astype('int')==cnum)
+                incat=incat[expr]
+        elif 'noise%i'%numNoiseZone in cut:
+            [ccol,clow,chigh]=cval # clow < noise < chigh
+            noises=numpy.power(incat[:,ccol],-0.5) # w -> rms
+            expr=numpy.where(noises>clow) and numpy.where(noises<chigh)
+            incat=incat[expr]
+
+    return incat
+
+#-------------------------------------------------------------------------------
 
