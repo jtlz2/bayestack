cc423db3174a0390a6ae373e5a393aa3462ac040
jtlz2 <jtlz2@astro.columbia.edu>
2015-10-30 12:43:37 +0200

    More updates to LF utility funcs - added erfss function; added redshift evolution term; need to finish off

--- lumfuncUtils.py
+++ lumfuncUtils.py
@@ -22,7 +22,8 @@ from profile_support import profile
 from utils import sqDeg2sr,sqrtTwo,find_nearest,medianArray,\
                            interpol,buildCDF,Jy2muJy,interpola
 from countUtils import powerLawFuncWrap
-from dnds_lumfunc import *
+from cosmocalc import cosmocalc
+import dnds_lumfunc
 
 if 'chains' in sys.argv[-1]:
     potential_settings=glob.glob(os.path.join(sys.argv[-1],'*settings*py'))
@@ -39,6 +40,40 @@ except:
     print '***Warning: Settings not loaded'
 
 #-------------------------------------------------------------------------------
+
+# LF utility functions
+def erfss(S,Sbinlow,Sbinhigh,ssigma):
+    return 0.5*(erf((S-Sbinlow)/(sqrtTwo*ssigma)) - erf((S-Sbinhigh)/(sqrtTwo*ssigma)))
+
+#-------------------------------------------------------------------------------
+
+@profile
+def dNdS_LF(S,z,params=None,paramsList=None,inta=None,area=None):
+    """
+    Source count model
+    S is in Jy
+    """
+
+    Lmin=Lmax=Lnorm=Lstar=Lslope=Lzevol=-99.0
+    Lmin=params[paramsList.index('LMIN')]
+    Lmax=params[paramsList.index('LMAX')]
+    Lnorm=params[paramsList.index('LNORM')]
+    Lstar=params[paramsList.index('LSTAR')]
+    Lslope=params[paramsList.index('LSLOPE')]
+    Lzevol=params[paramsList.index('LZEVOL')]
+
+    dl=cosmocalc(z,H0=Ho,WM=wm)['DL_Mpc']
+    [Smin,Smax]=dnds_lumfunc.get_sbins([Lmin,Lmax],z,dl)
+
+    if Smin < S < Smax:
+        if inta is not None:
+            return float(inta(S))
+        else:
+            L=dnds_lumfunc.get_Lbins([S],z,dl)
+            Lbins,log10(phi)=schechter(L,Lstar,Lslope,Lnorm)
+            return (1.0+z)**Lzevol*10**phi[0]
+    else:
+        return 0.0
 
 
 #-------------------------------------------------------------------------------
@@ -61,7 +96,7 @@ def calculateIL(params,paramsList,bins=None,area=None,
         Lslope=params[paramsList.index('LSLOPE')]
         Lzevol=params[paramsList.index('LZEVOL')]
         noise=params[paramsList.index('noise')]
-
+        redshift=?????
         #for i in range(6): print params[i]
         nbins=len(bins)-1
         II = numpy.zeros(nbins)
@@ -75,60 +110,20 @@ def calculateIL(params,paramsList,bins=None,area=None,
 
 @profile
 def calculateL3(params,paramsList,bins=None,area=None,\
-                family=None,dump=None,verbose=False):
+                family=None,dump=None,verbose=False,inta=None,doRayleigh=False):
 
     """
-    Do this for all bins simultaneously
-    Mimic pn_integral.pro:
-    ;+                                                                              
-    ;                                                                               
-    ; Ketron 11/2012                                                                
-    ;                                                                               
-    ; Integration of eq. (7).                                                       
-    ;                                                                               
-    ;                                                                               
-    ;    /smax               /max_bin                                               
-    ;    |     dS * dN/dS *  |        dS' exp(-(S-S')/2/rms^2)                      
-    ;    /smin               /min_bin                                               
-    ;                                                                               
-    ; Analytic solution of second integral is error functions.                      
-    ;                                                                               
-    ;-  
+    For LF,
+    function to calculate mock data for a given power law or interpolation object
     """
-
-    C=alpha=Smin=Smax=beta=S0=gamma=S1=delta=S2=-99.0
-
-    nlaws=int(0.5*len(paramsList)-1)
-
-    C=params[paramsList.index('C')]
-    Smin=params[paramsList.index('S0')]
-    alpha=params[paramsList.index('a0')]
-    if nlaws > 1:
-        beta=params[paramsList.index('a1')]
-        S0=params[paramsList.index('S1')]
-    if nlaws > 2:
-        gamma=params[paramsList.index('a2')]
-        S1=params[paramsList.index('S2')]
-    if nlaws > 3:
-        delta=params[paramsList.index('a3')]
-        S2=params[paramsList.index('S3')]
-
-    iSmax=int([i for i in paramsList if i.startswith('S')][-1][-1])
-    Smax=params[paramsList.index('S%i'%iSmax)]
-
-    noise=params[paramsList.index('noise')]
 
     nbins=len(bins)
     II = numpy.zeros(nbins-1)
-    II2 = numpy.zeros(nbins-1)
     for ibin in xrange(nbins-1):
         sqDeg2srr=sqDeg2sr
         #sqDeg2srr=1.0
-        if nlaws == 1:
-            II[ibin]=integrate.quad(lambda S:powerLawFuncErrorFn(S,C*1.0e-6*10**(-6.0*alpha),alpha,Smin,Smax,bins[ibin],bins[ibin+1],noise,sqDeg2srr*area),Smin,Smax)[0]
-            II2[ibin]=integrate.quad(lambda S:powerLawFuncErrorFn(S,C,alpha,Smin/1.0e6,Smax/1.0e6,bins[ibin]/1.0e6,bins[ibin+1]/1.0e6,noise/1.0e6,sqDeg2srr*area),Smin/1.0e6,Smax/1.0e6)[0]
-        elif nlaws in [2,3,4]:
-            II[ibin]=integrate.quad(lambda S:powerLawFuncErfsS(S,nlaws,C,alpha,-99.0,beta,Smin/1.0e6,Smax/1.0e6,bins[ibin]/1.0e6,bins[ibin+1]/1.0e6,S0/1.0e6,gamma,S1/1.0e6,delta,S2/1.0e6,noise/1.0e6,sqDeg2srr*area),Smin/1.0e6,Smax/1.0e6)[0]
+        #print area,sqDeg2sr
+        II[ibin]=IL(dNdS_LF,params,paramsList,bins[ibin],bins[ibin+1],inta=inta,area=sqDeg2srr*area)
 
     return II
 
