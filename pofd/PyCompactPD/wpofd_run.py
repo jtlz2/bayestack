0327edfef82cddfae2e3c96441f44f6259202f1b
jtlz2 <jtlz2@astro.columbia.edu>
2016-07-13 13:23:14 +0200

    tidying

--- pofd/PyCompactPD/wpofd_run.py
+++ pofd/PyCompactPD/wpofd_run.py
@@ -18,17 +18,17 @@ def main():
     Nbins=data.shape[0]
     dd=np.ascontiguousarray(data.T)
     DataArray = ffi.cast("double *",dd.ctypes.data)
-    #xprint dd
+
     if '2' in VERSION:
         """
         Takes two arrays x, y for the model coordinates
         """
         x=[-7.32,-6.7,-6.3,-5.533,-4.766,-4.0,-3.25,-1.9]
         y=[16.1737,15.0620,14.4342,13.4563,12.1586,10.2764,8.55,6.314]
-        #y=range(10,18)
 
         xy=np.array([x,y])
         i_p=ffi.cast("double *",np.ascontiguousarray(xy).ctypes.data)
+        i_l=xy.shape[-1]
 
         ParamsArray = ffi.new("struct PD_params *",{\
         'd_max':1.0e-3,
@@ -38,7 +38,7 @@ def main():
         'PSFresultionFWHM':6.0,
         'pixelsize':1.0,
         'sigma_noise':17.0e-6,
-        'interplot_length':8,
+        'interplot_length':i_l,
         'interplot_pointer':i_p})
 
         z=np.zeros(Nbins)
