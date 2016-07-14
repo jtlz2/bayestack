21d4fcdaac7713fed5a16822cc0e0e3e2297e2e5
jtlz2 <jtlz2@astro.columbia.edu>
2016-07-14 14:25:59 +0200

    added loglike functions

--- pofd/PyCompactPD/wpofd_run.py
+++ pofd/PyCompactPD/wpofd_run.py
@@ -4,7 +4,46 @@ import os,sys
 import numpy as np
 from _wpofd import ffi,lib
 
+from math import pi
+sqDeg2sr=4.0*pi*pi/129600.0
+
 VERSION='_v2' # or ''
+
+#-------------------------------------------------------------------------------
+
+def poissonLhood(data,realisation,silent=True):
+    if not silent:
+        for i in range(len(data)):
+            print i,data[i],realisation[i]
+    kk=data[np.where(realisation > 0) and np.where(data>0)];
+    iii=realisation[np.where(realisation > 0) and np.where(data>0)]
+    loglike = (kk*np.log(iii) + kk - kk*np.log(kk) - iii).sum()
+    return loglike
+
+#-------------------------------------------------------------------------------
+
+def loglike_pofd(data,model,ParamsArray):
+    """
+    """
+
+    Nbins=data.shape[0]
+    dd=np.ascontiguousarray(data.T)
+    DataArray = ffi.cast("double *",dd.ctypes.data)
+
+    xy=model
+
+    i_p=ffi.cast("double *",np.ascontiguousarray(xy).ctypes.data)
+    i_l=xy.shape[-1]
+
+    z=np.zeros(Nbins)
+    result=ffi.cast("double *",np.ascontiguousarray(z).ctypes.data)
+    r=lib.CompactPD_LH(Nbins,DataArray,result,ParamsArray)
+
+    realisation=np.array([result[i] for i in range(Nbins)])
+
+    loglike=poissonLhood(data[:,-1],np.power(10,realisation),silent=True)
+    
+    return loglike
 
 #-------------------------------------------------------------------------------
 
@@ -15,6 +54,7 @@ def main():
     histof=os.path.join(datadir,histof)
     data=np.loadtxt(histof)
 
+    Npix=data[:,-1].sum()
     Nbins=data.shape[0]
     dd=np.ascontiguousarray(data.T)
     DataArray = ffi.cast("double *",dd.ctypes.data)
@@ -23,12 +63,20 @@ def main():
         """
         Takes two arrays x, y for the model coordinates
         """
-        x=[-7.32,-6.7,-6.3,-5.533,-4.766,-4.0,-3.25,-1.9]
-        y=[16.1737,15.0620,14.4342,13.4563,12.1586,10.2764,8.55,6.314]
+        #x=[-7.32,-6.7,-6.3,-5.533,-4.766,-4.0,-3.25,-1.9]
+        #y=[16.1737,15.0620,14.4342,13.4563,12.1586,10.2764,8.55,6.314]
 
-        xy=np.array([x,y])
-        i_p=ffi.cast("double *",np.ascontiguousarray(xy).ctypes.data)
-        i_l=xy.shape[-1]
+        # These are very roughly the SKADS counts
+        fluxes=[2.0e-8,17.0e-6,85.0e-6]
+        dnds_eucl_per_sr=[5.0e-3,1.0,3.0]
+
+        x=np.log10(np.array(fluxes))
+        y=np.log10(np.array(dnds_eucl_per_sr)*np.power(np.power(10,x),-2.5))
+
+        model=np.array([x,y])
+        #np.savetxt('xy.txt',model.T)
+        i_p=ffi.cast("double *",np.ascontiguousarray(model).ctypes.data)
+        i_l=model.shape[-1]
 
         ParamsArray = ffi.new("struct PD_params *",{\
         'd_max':1.0e-3,
@@ -45,8 +93,15 @@ def main():
         result=ffi.cast("double *",np.ascontiguousarray(z).ctypes.data)
         r=lib.CompactPD_LH(Nbins,DataArray,result,ParamsArray)
 
-        loglike=result[0] # Dereferencing trick, i.e. *p
-        print '\nresult: %f [unchecked]\n' % loglike
+        realisation=np.array([result[i] for i in range(Nbins)])
+
+        loglike=poissonLhood(data[:,-1],np.power(10,realisation),silent=True)
+        print 'loglike = %f [unchecked]\n'%loglike
+
+        loglike=loglike_pofd(data,model,ParamsArray)
+        print 'loglike = %f [unchecked]\n'%loglike
+        
+        return 0
 
     else:
         """
